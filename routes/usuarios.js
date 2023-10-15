e3def58a62c0ceb25578c2b5431e02d22e953a3f
Gustavo <gustavotet2022@outlook.com.br>
2023-10-15 00:20:12 -0300

    Cadastro pronto, login ainda em andamento

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -16,29 +16,57 @@ router.get('/', (req, res, next) => {
 router.post('/cadastro', (req, res, next) => {
     mysql.getConnection((error, conn) => {
         if(error) {return res.status(500).send({error:error})}
-        bcrypt.hash(req.body.senha, 10, (errBcrypt, hash) => {
-            if(err){return res.status(500).send({error: errBcrypt})}
-            conn.query('INSERT INTO usu_usuarios (usu_nome, usu_email, usu_senha) VALUES (?,?,?)', [req.body.nome, req.body.email, hash], 
-            (error,results) => {
-                conn.release();
-                if(error) {return res.status(500).send({error:error})}
-                response = {
-                    mensagem: "Usuário cadastrado!",
-                    usuariocriado: {
-                        usu_id : results.insertId,
-                        nome: req.body.nome,
-                        email: req.body.email
-                    }
-                }
-                return res.status(201).send(response);
-            })
-
+        //Se já houver email cadastrado
+        conn.query('SELECT * FROM usu_usuario WHERE usu_email = ?', [req.body.email], (error,results) => {
+            if(error){return res.status(500).send({error: error})}
+            if(results.length > 0) {
+                res.status(409).send({mensagem: 'usuario já cadastrado!'});
+            }else{
+                bcrypt.hash(req.body.senha, 10, (errBcrypt, hash) => {
+                    if(error){return res.status(500).send({error: errBcrypt})}
+                    conn.query('INSERT INTO usu_usuario (usu_nome, usu_email, usu_senha) VALUES (?,?,?)', [req.body.nome, req.body.email, hash], 
+                    (error,results) => {
+                        conn.release();
+                        if(error) {return res.status(500).send({error:error})}
+                        response = {
+                            mensagem: "Usuário cadastrado!",
+                            usuariocriado: {
+                                usu_id : results.insertId,
+                                nome: req.body.nome,
+                                email: req.body.email
+                            }
+                        }
+                        return res.status(201).send(response);
+                    });
+                });
+            }
         })
-
-    })
-
-})
-
-
     
+    });
+});
+
+router.post('/login', (req, res, next) =>{
+    mysql.getConnection((error,conn) =>{
+        if(error) {return res.status(500).send({error: error})}
+        const query = 'SELECT * FROM usu_usuario WHERE usu_email = ?';
+        conn.query(query, [req.body.email], (error, results, fields) =>{
+            conn.release();
+            if(error) {return res.status(500).send({error: error})};
+            if(results.length < 1) {
+                return res.status(401).send({mensagem: 'falha na autenticação'});
+            }
+           bcrypt.compare(req.body.senha, results[0].senha, (err, result) => {
+            if(err){
+                return res.status(401).send({mensagem: 'falha na autenticação'});
+            }
+            if(result){
+                return res.status(200).send({mensagem: 'Autenticado com sucesso'});
+            }
+            return res.status(401).send({mensagem : 'falha na autenticação'})
+
+           });
+        });
+    });
+});
+
 module.exports = router;
