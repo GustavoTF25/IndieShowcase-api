914dcd00b78999fe8328c6c102029bdf5ffcc2ec
Gustavo <gustavotet2022@outlook.com.br>
2023-11-28 13:57:03 -0300

    melhorias de segurança e performance

--- app.js
+++ app.js
@@ -8,6 +8,13 @@ const rotaCategorias = require('./routes/categorias');
 const fileUpload = require('express-fileupload');
 
 
+app.use((req, res, next) => {
+    res.setHeader('X-Content-Type-Options', 'nosniff');
+    res.setHeader('X-Frame-Options', 'SAMEORIGIN');
+    res.setHeader('Strict-Transport-Security', 'max-age=31536000; includeSubDomains; preload');
+    res.setHeader('X-XSS-Protection', '1; mode=block');
+    next();
+});
 app.use(morgan('dev'));
 //app.use('/postagens/uploads', express.static('uploads'))
 app.use('/postagens', express.static('postagens'));
--- server.js
+++ server.js
@@ -1,8 +1,34 @@
+const cluster = require('cluster');
 const http = require('http');
 const cors = require('cors');
 const app = require('./app');
 require('dotenv').config();
-const port = process.env.PORT || 8000;
-const server = http.createServer(app);
-server.listen(port);
 
+if (cluster.isMaster) {
+    // Este bloco é executado apenas no processo mestre
+
+    // Obtém o número de núcleos na CPU
+    const numCPUs = require('os').cpus().length;
+
+    // Cria processos filhos igual ao número de núcleos
+    for (let i = 0; i < numCPUs; i++) {
+        cluster.fork();
+    }
+
+    // Evento que é acionado quando um processo filho morre
+    cluster.on('exit', (worker, code, signal) => {
+        console.log(`Worker ${worker.process.pid} morreu`);
+        // Reinicia o processo filho que morreu
+        cluster.fork();
+    });
+} else {
+    // Configuração do servidor HTTP
+    const port = process.env.PORT || 8000;
+    const server = http.createServer(app);
+    //server.maxConnections = 15000;
+
+    // Inicia o servidor
+    server.listen(port, () => {
+        console.log(`Servidor rodando na porta ${port} no processo ${process.pid}`);
+    });
+}
