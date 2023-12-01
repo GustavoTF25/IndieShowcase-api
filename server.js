2441150061ca9b0f3acdddbd868e40712a5bc80f
Gustavo <gustavotet2022@outlook.com.br>
2023-12-01 17:41:48 -0300

    Alteração pontual

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ require('dotenv').config();
     const port = process.env.PORT || 8000;
     const server = http.createServer(app);
     //server.maxConnections = 15000;
-
+    
     // Inicia o servidor
     server.listen(port, () => {
         console.log(`Servidor rodando na porta ${port} no processo ${process.pid}`);
