748bef3fab6907aa73a2b7c0fc9077733c9b764b
Gustavo <gustavotet2022@outlook.com.br>
2023-12-01 17:40:07 -0300

    Melhoria de Performance

--- server.js
+++ server.js
@@ -1,22 +1,8 @@
-const cluster = require('cluster');
 const http = require('http');
 const cors = require('cors');
 const app = require('./app');
 require('dotenv').config();
 
-if (cluster.isMaster) {
-
-    const numCPUs = require('os').cpus().length;
-
-    for (let i = 0; i < numCPUs; i++) {
-        cluster.fork();
-    }
-
-    cluster.on('exit', (worker, code, signal) => {
-        console.log(`Worker ${worker.process.pid} morreu`);
-        cluster.fork();
-    });
-} else {
     // Configuração do servidor HTTP
     const port = process.env.PORT || 8000;
     const server = http.createServer(app);
@@ -26,4 +12,3 @@ if (cluster.isMaster) {
     server.listen(port, () => {
         console.log(`Servidor rodando na porta ${port} no processo ${process.pid}`);
     });
-}
