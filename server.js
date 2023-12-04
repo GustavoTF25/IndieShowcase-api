75e0c6b9d8580c72ee741e254e1e0fe5a8f9be7c
Gustavo <gustavotet2022@outlook.com.br>
2023-12-04 07:42:17 -0300

    Alteração pontual

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const cors = require('cors');
 const app = require('./app');
 require('dotenv').config();
 
-    // Configuração do servidor HTTP
-    const port = process.env.PORT || 8000;
-    const server = http.createServer(app);
-    //server.maxConnections = 15000;
-    
-    // Inicia o servidor
-    server.listen(port, () => {
-        console.log(`Servidor rodando na porta ${port} no processo ${process.pid}`);
-    });
+// Configuração do servidor HTTP
+const port = process.env.PORT || 8000;
+const server = http.createServer(app);
+//server.maxConnections = 15000;
+
+// Inicia o servidor
+server.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port} no processo ${process.pid}`);
+});
