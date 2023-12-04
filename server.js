465e5727b64f6606288088e391b30b0926c8454d
Gustavo <gustavotet2022@outlook.com.br>
2023-12-04 07:43:54 -0300

    Removido limite de requisições

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ require('dotenv').config();
 // Configuração do servidor HTTP
 const port = process.env.PORT || 8000;
 const server = http.createServer(app);
-//server.maxConnections = 15000;
 
 // Inicia o servidor
 server.listen(port, () => {
